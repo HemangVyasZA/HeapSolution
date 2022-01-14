535b8d74719dc03fafbfa64f733f4930278cf1aa
HemangVyasZA <32388243+HemangVyasZA@users.noreply.github.com>
2022-01-14 18:24:47 +0200

    Add project files.

---
+++ HeapTree.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeapSolution
+{
+   public class HeapTree
+   {
+        private const int DEFAULT_CAPACITY = 7;
+        private readonly IComparer<int> comparer;
+        private int[] data;
+        private int capacity;
+        public int Count { get; private set; }
+        public int[] HeapContents { get; private set; }
+
+        public HeapTree(): this(Comparer<int>.Default)
+        {
+
+        }
+
+        public HeapTree(IComparer<int> comparer)
+        {
+            this.comparer = comparer;
+            capacity = DEFAULT_CAPACITY;
+            data = new int[capacity];
+        }
+
+        public void Push(int value)
+        {
+            ExpandArrayIfNeeded();
+
+            // Step – 1 Add the new value at the end of the heap array. This will ensure that tree is complete.
+            data[Count] = value;
+            int ChildIndex = Count;
+
+            // Step - 2 While heap property is not satisfied, swap the new item with its parent.
+            while (ChildIndex > 0 && IsFirstElementGreaterThanSecondElement(ChildIndex, GetParentIndex(ChildIndex)))
+            {
+                Swap(ChildIndex, GetParentIndex(ChildIndex));
+                ChildIndex = GetParentIndex(ChildIndex);
+            }
+
+            HeapContents = data;
+            Count++;
+        }
+
+        public void Pop()
+        {
+            if (Count <= 1)
+            {
+                Count = 0;
+                return;
+            }
+
+            // Step – 1 Move the last item (the right most) in the heap array into the zero (root) index. 
+            data[0] = data[Count - 1];
+            data[Count - 1] = 0;
+            Count--;
+
+            // Step - 2 While heap property is not satisfied, swap the item with one of its children if needed.
+            PushDownIfNeeded(0);
+        }
+        private void PushDownIfNeeded(int parentIndex)
+        {
+            int leftChildIndex = GetLeftChildIndex(parentIndex);
+            int rightChildIndex = GetRightChildIndex(parentIndex);
+
+            // case - 1 : no child
+            if (leftChildIndex >= Count)
+            {
+                return;
+            }
+
+            // case - 2 : Only Left child
+            if (rightChildIndex >= Count)
+            {
+                if (IsFirstElementGreaterThanSecondElement(leftChildIndex, parentIndex))
+                {
+                    Swap(leftChildIndex, parentIndex);
+                    PushDownIfNeeded(leftChildIndex);
+                    return;
+                }
+            }
+
+            // case - 3 : Two children left and right
+            // if left child is greater than right child, swap left child with parent
+            if (IsFirstElementGreaterThanSecondElement(leftChildIndex, rightChildIndex))
+            {
+                if (IsFirstElementGreaterThanSecondElement(leftChildIndex, parentIndex))
+                {
+                    Swap(leftChildIndex, parentIndex);
+                    PushDownIfNeeded(leftChildIndex);
+                    return;
+                }
+            }
+            //if right child is greater than left child, swap right child with parent
+            else if(IsFirstElementGreaterThanSecondElement(rightChildIndex,parentIndex))
+            {
+                Swap(rightChildIndex, parentIndex);
+                PushDownIfNeeded(rightChildIndex);
+                return;
+            }
+        }
+
+        private int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        private int GetLeftChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 1;
+        }
+
+        private int GetRightChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 2;
+        }
+
+        private void Swap(int left, int right)
+        {
+            int temp = data[left];
+            data[left] = data[right];
+            data[right] = temp;
+        }
+
+        private bool IsFirstElementGreaterThanSecondElement(int FirstElementIndex, int SecondElementIndex)
+        {
+            return data[FirstElementIndex] > data[SecondElementIndex];
+        }
+        private void ExpandArrayIfNeeded()
+        {
+            if (Count == capacity)
+            {
+                capacity = Count * 2;
+                var newHeapTree = new int[capacity];
+                for (int counter = 0; counter < data.Length; counter++)
+                {
+                    newHeapTree[counter] = data[counter];
+                }
+                data = newHeapTree;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace HeapSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //HeapTree heapTree = new HeapTree();
+            ////heapTree.Push(8);
+            ////heapTree.Push(5);
+            ////heapTree.Push(6);
+            ////heapTree.Push(4);
+            ////heapTree.Push(2);
+            ////heapTree.Push(9);
+            ////heapTree.Push(3);
+            ////heapTree.Push(12);
+            ////heapTree.Push(10);
+            ////heapTree.Push(7);
+            ////heapTree.Push(1);
+            ////heapTree.Push(14);
+            //heapTree.Push(9);
+            //heapTree.Push(7);
+            //heapTree.Push(8);
+            //heapTree.Push(4);
+            //heapTree.Push(2);
+            //heapTree.Push(6);
+            //foreach (var item in heapTree.HeapContents)
+            //{
+            //    Console.Write($"{item}\t");
+            //}
+
+            //heapTree.Pop();
+            //heapTree.Pop();
+            //Console.WriteLine();
+            //Console.WriteLine($"Count is {heapTree.Count}");
+            //foreach (var item in heapTree.HeapContents)
+            //{
+            //    Console.Write($"{item}\t");
+            //}
+
+            int[] arr = { 74, 19, 24, 5, 8, 79, 42, 15, 20, 53, 11 };
+
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Applying Heap Sort....");
+            HeapSort(arr);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.ReadLine();
+        }
+
+        static void HeapSort(int[] arrayToBeSorted)
+        {
+            int lengthOfArray = arrayToBeSorted.Length;
+
+            // Build a max-heap from an unordered array.
+            // we do NOT need to consider leaf nodes. that's why we start from  lengthOfArray / 2 - 1
+            // this will set max element at 0th index
+            for ( int counter = lengthOfArray / 2 - 1;  counter >= 0; counter--)
+            {
+                Heapify(arrayToBeSorted, lengthOfArray, counter);
+            }
+
+            // now pop up the last element. swap the last element with the max element that is the 0th index
+            for (int counter = lengthOfArray - 1; counter >= 0; counter--)
+            {
+                int temp = arrayToBeSorted[0];
+                arrayToBeSorted[0] = arrayToBeSorted[counter];
+                arrayToBeSorted[counter] = temp;
+                // Now run Heapify on the heap in case the new root causes a violation of the max-heap property. (Its children will still be max-heaps.)
+                Heapify(arrayToBeSorted, counter, 0);
+            }
+        }
+        static void Heapify(int[] arrayToBeSorted, int lengthOfArray, int indexOfLargestElement)
+        {
+            // Step - 1: grab the index of largest element and its children
+            int indexOfCurrentLargestElement = indexOfLargestElement;
+            int leftChildIndex = 2 * indexOfLargestElement + 1;
+            int rightChildIndex = 2 * indexOfLargestElement + 2;
+
+            // Step - 2: check if left child is bigger than the largest element? set index of current largest element  to left child's index
+            // set left child's index to index of current largest element
+            if (leftChildIndex < lengthOfArray && arrayToBeSorted[leftChildIndex] > arrayToBeSorted[indexOfCurrentLargestElement])
+            {
+                indexOfCurrentLargestElement = leftChildIndex;
+            }
+
+            // Step - 3: check if right child is bigger than the largest element
+            // set right child's index to index of current largest element
+            if (rightChildIndex < lengthOfArray && arrayToBeSorted[rightChildIndex] > arrayToBeSorted[indexOfCurrentLargestElement] )
+            {
+                indexOfCurrentLargestElement = rightChildIndex;
+            }
+
+            // Step - 4 : if you found new largest element, swap it and run heapify again for that new largest element.
+            if (indexOfCurrentLargestElement != indexOfLargestElement)
+            {
+                int temp = arrayToBeSorted[indexOfLargestElement];
+                arrayToBeSorted[indexOfLargestElement] = arrayToBeSorted[indexOfCurrentLargestElement];
+                arrayToBeSorted[indexOfCurrentLargestElement] = temp;
+                Heapify(arrayToBeSorted, lengthOfArray, indexOfCurrentLargestElement);
+            }
+        }
+    }
+}
